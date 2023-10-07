4f21fad6591bb5a15c64a726f4d6945a93d1b746
Chani17 <cucute8878@naver.com>
2023-10-07 18:14:17 +0900

    Fix: Remove unnecessary mapping elements with student entity

--- src/main/java/inu/thebite/toryaba/repository/StoRepository.java
+++
@@ -1,9 +0,0 @@
-package inu.thebite.toryaba.repository;
-
-import inu.thebite.toryaba.entity.Sto;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StoRepository extends JpaRepository<Sto, Long> {
-}
